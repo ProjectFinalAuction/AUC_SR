8cfffbb2367c3eed999ab746ba618eff18a4106e
EAN SOKCHOMRERN <chomrern@gmail.com>
2016-08-08 10:10:49 +0700

    add more

--- src/main/java/org/khmeracademy/auction/configuration/AUCConfiguration.java
+++ src/main/java/org/khmeracademy/auction/configuration/AUCConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 @MapperScan("org.khmeracademy.auction.repositories")
 public class AUCConfiguration {
 		@Bean 
