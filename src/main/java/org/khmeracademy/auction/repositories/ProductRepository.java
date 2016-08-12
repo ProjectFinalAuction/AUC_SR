c244944c761fb6891e4b73e644243a149fd6253e
sopheakreth <rethsopheak@gmail.com>
2016-08-12 14:43:47 +0700

    update productrepository

--- src/main/java/org/khmeracademy/auction/repositories/ProductRepository.java
+++ src/main/java/org/khmeracademy/auction/repositories/ProductRepository.java
@@ -19,16 +19,19 @@ public interface ProductRepository {
 	@Select(R_PRODUCTS)
 	@Results(value={
 			// supplier
+			@Result(property="supplier.supplier_id",column="supplier_id"),
 			@Result(property="supplier.contact_name",column="contact_name"),
 			@Result(property="supplier.address",column="address"),
 			@Result(property="supplier.phone",column="phone"),
 			@Result(property="supplier.email",column="email"),
 			
 			// category
+			@Result(property="category.category_id",column="category_id"),
 			@Result(property="category.category_name",column="category_name"),
 			@Result(property="category.category_description",column="category_description"),
 			
 			// brand
+			@Result(property="brand.brand_id",column="brand_id"),
 			@Result(property="brand.brand_name",column="brand_name"),
 			@Result(property="brand.brand_description",column="brand_description")			
 	})
